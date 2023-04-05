8418da27f697ee166fb26ca8a5fb524f70e74497
Tankist_Vitalia <Tankist_Vitalia@DESKTOP-6FFJIIE>
2023-04-06 01:03:28 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+namespace Vitaliy.Helloworld
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Hello, C#");
+            Thread.Sleep(2000);
+            Console.Clear();
+            Console.WriteLine("Hello, Dude");
+            Thread.Sleep(2000);
+            Console.Clear();
+            Console.WriteLine("Hello, Liza");
+            Thread.Sleep(2000);
+            Console.Clear();
+            Console.WriteLine("Hello, World");
+            Thread.Sleep(2000);
+            Console.Clear();
+            Console.ReadLine();
+        }
+    }
+}
